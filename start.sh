3e9a0ce4231e5bba2af101588210ed08c64c620a
mpous <marc@balena.io>
2020-06-18 20:03:13 +0200

    new logs

--- start.sh
+++ start.sh
@@ -3,11 +3,16 @@ TAG_KEY="CPU_Temp"
 
 while : ; do
 	TEMP=$(expr $(cat /sys/class/thermal/thermal_zone0/temp) / 1000)
+	echo $TEMP
+
+	DATAJSON="{\"data\":{\"path\":\"cpu_temp\", \"method\": \"post\",\"body\":{\"temp\":\"$TEMP\"} }, \"applicationId\": \"\", \"applicationName\": \"Humpback_flow\", \"triggerId\":\"\", \"triggerType\":\"\", \"flowId\":\"\", \"flowName\":\"\", \"globals\":{}  }"
+
+	echo $DATAJSON
 
 	curl -sX POST \
 	"https://triggers.losant.com/webhooks/5QoLVhYVZREuKZO9oKqvk5F-UVgjOiOj-tlDwFegaA1$" \
 	-H "Content-Type: application/json" \
-	--data "{ \"device\": \"$ID\", \"tag_key\": \"$TAG_KEY\", \"value\": \"$TEMP\" }" > /dev/null
+	--data ${DATAJSON} 
 
 	sleep 60
 done
