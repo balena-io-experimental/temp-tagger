6482b93f911dce0916a55b21ab15db8d0f12b9c2
mpous <marc@balena.io>
2020-07-02 19:00:40 +0200

    CURL to Losant

--- start.sh
+++ start.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
+
 TAG_KEY="CPU_Temp"
 
 while : ; do
 	TEMP=$(expr $(cat /sys/class/thermal/thermal_zone0/temp) / 1000)
+	DATAJSON="{\"data\":{\"path\":\"/example/path\", \"method\": \"POST\",\"body\":{\"temp\":\"$TEMP\"} }, \"applicationId\": \"5ef35e4682043900066427db\", \"applicationName\": \"Balena/Losant Template\", \"triggerId\":\"5ef361350515e2000662a99a\", \"triggerType\":\"webhook\", \"flowId\":\"5ef361ec0515e2000662a99e\", \"flowName\":\"Rpi Temp\", \"globals\":{}  }"
+
 	echo $TEMP
 
-	DATAJSON="{\"data\":{\"path\":\"cpu_temp\", \"method\": \"post\",\"body\":{\"temp\":\"$TEMP\"} }, \"applicationId\": \"\", \"applicationName\": \"Humpback_flow\", \"triggerId\":\"\", \"triggerType\":\"\", \"flowId\":\"\", \"flowName\":\"\", \"globals\":{}  }"
-
-	echo $DATAJSON
-
-	curl -sX POST \
-	"https://triggers.losant.com/webhooks/5QoLVhYVZREuKZO9oKqvk5F-UVgjOiOj-tlDwFegaA1$" \
+	LOSANT_CURL = $(curl --write-out "%{http_code}" -sX POST "$LOSANT_WEBHOOK" \
 	-H "Content-Type: application/json" \
-	--data ${DATAJSON} 
+	--data "$DATAJSON")
 
 	sleep 60
 done
