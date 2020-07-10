e09215952a01a195b0bddfc00251caddf68761b5
mpous <marc@balena.io>
2020-07-10 14:31:12 +0200

    json simplification

--- start.sh
+++ start.sh
@@ -6,7 +6,8 @@ echo $TTN_EUI
 
 while : ; do
 	TEMP=$(expr $(cat /sys/class/thermal/thermal_zone0/temp) / 1000)
-	DATAJSON="{\"data\":{\"path\":\"/example/path\", \"method\": \"POST\",\"body\":{\"temp\":\"$TEMP\", \"deviceId\":\"$TTN_EUI\"} }, \"applicationId\": \"5ef35e4682043900066427db\", \"applicationName\": \"Balena/Losant Template\", \"triggerId\":\"5ef361350515e2000662a99a\", \"triggerType\":\"webhook\", \"flowId\":\"5ef361ec0515e2000662a99e\", \"flowName\":\"Rpi Temp\", \"globals\":{}  }"
+	#DATAJSON="{\"data\":{\"path\":\"/example/path\", \"method\": \"POST\",\"body\":{\"temp\":\"$TEMP\", \"deviceId\":\"$TTN_EUI\"} }, \"applicationId\": \"5ef35e4682043900066427db\", \"applicationName\": \"Balena/Losant Template\", \"triggerId\":\"5ef361350515e2000662a99a\", \"triggerType\":\"webhook\", \"flowId\":\"5ef361ec0515e2000662a99e\", \"flowName\":\"Rpi Temp\", \"globals\":{}  }"
+	DATAJSON="{\"data\":{\"method\": \"POST\",\"body\":{\"temp\":\"$TEMP\", \"deviceId\":\"$TTN_EUI\"} } }"
 
 	echo $TEMP
 
