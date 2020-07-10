3a7dec157349041b439f3b2cf9f06b842c312a5a
mpous <marc@balena.io>
2020-07-10 15:24:00 +0200

    json simplification

--- start.sh
+++ start.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-TTN_EUI=$(cat /sys/class/net/eth0/address | sed -r 's/[:]+//g' | sed -e 's#\(.\{6\}\)\(.*\)#\1FFFE\2#g')
-
-echo $TTN_EUI
-
 while : ; do
 	TEMP=$(expr $(cat /sys/class/thermal/thermal_zone0/temp) / 1000)
-	#DATAJSON="{\"data\":{\"path\":\"/example/path\", \"method\": \"POST\",\"body\":{\"temp\":\"$TEMP\", \"deviceId\":\"$TTN_EUI\"} }, \"applicationId\": \"5ef35e4682043900066427db\", \"applicationName\": \"Balena/Losant Template\", \"triggerId\":\"5ef361350515e2000662a99a\", \"triggerType\":\"webhook\", \"flowId\":\"5ef361ec0515e2000662a99e\", \"flowName\":\"Rpi Temp\", \"globals\":{}  }"
-	DATAJSON="{\"data\":{\"method\": \"POST\",\"body\":{\"temp\":\"$TEMP\", \"deviceId\":\"$TTN_EUI\"} } }"
+	DATAJSON="{\"temp\":\"$TEMP\", \"balena-uuid\":\"$BALENA_DEVICE_UUID\", \"balena-name\":\"$BALENA_DEVICE_NAME_AT_INIT\"}"
 
 	echo $TEMP
 
